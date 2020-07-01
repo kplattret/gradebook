35423b08bffb2681b8309922f35a4288c708b986
Kevin Plattret <kevin@plattret.com>
2020-07-01 09:08:07 +0100

    Variables, types, arrays and loops
    Introduces a fixed-size array of grades, which have the implicit double
    type. We can simply use `var` when giving a value to a variable, which
    automatically sets the data type for us. In the same way, we can use
    `new[]` as opposed to `new double[]` as we set the value directly.
    
    Uses a `foreach` loop to add-up all the numbers in our array and return
    a result in the console, as a double.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,6 +6,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+            var result = 0.0;
+
+            foreach(double number in numbers)
+            {
+                result += number;
+            }
+
+            Console.WriteLine(result);
+
             if(args.Length > 0) {
                 Console.WriteLine($"Hello {args[0]}!");
             } else {
