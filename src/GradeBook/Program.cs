cc86f008b5c9a5900d32ffbf4828b78a8ea9d41d
Kevin Plattret <kevin@plattret.com>
2020-07-01 09:19:56 +0100

    Using a list
    Introduces a list of doubles, which allows us to store any number of
    values, since we don't know how many grades we will have in the end.
    Updates the output and ensures that the average only has one decimal.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,18 +7,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
-            var result = 0.0;
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
-            foreach(double number in numbers)
+            var result = 0.0;
+            foreach(double number in grades)
             {
                 result += number;
             }
-
-            Console.WriteLine(result);
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
 
             if(args.Length > 0) {
-                Console.WriteLine($"Hello {args[0]}!");
+                Console.WriteLine($"Hello, {args[0]}!");
             } else {
                 Console.WriteLine("Hello!");
             }
